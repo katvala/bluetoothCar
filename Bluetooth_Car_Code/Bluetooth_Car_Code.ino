c4b0335d5e43e8932250628f915f66bbd8c5502e
Kat <katherine.valcarcel1@usmp.pe>
2023-06-12 15:37:44 -0500

    V2

--- Bluetooth_Car_Code/Bluetooth_Car_Code.ino
+++ Bluetooth_Car_Code/Bluetooth_Car_Code.ino
@@ -6,9 +6,12 @@ const int driver_IN3 = 7;  // Pin que controla el sentido de giro
 const int driver_ENA = 5;  // Pin PWM que controla la velocidad
 const int driver_IN1 = 4;  // Pin que controla el sentido de giro del motor
 
-// Variables enteras que almacenan la velocidad del motor A y B
-int velocidad_motor_B = 0;
-int velocidad_motor_A = 0;
+const int led = 13;
+const int ledRED = 11;
+const int buzz = 12;
+
+// Variable entera que almacena la velocidad de los motores
+int velocidad_motor = 0;
 
 void setup() {
   // Configuramos los pines del driver como salida
@@ -16,10 +19,12 @@ void setup() {
   pinMode(driver_IN1, OUTPUT);
   pinMode(driver_ENB, OUTPUT);
   pinMode(driver_IN3, OUTPUT);
-  
+  pinMode(led, OUTPUT);
+  pinMode(buzz, OUTPUT);
+
   // Inicializamos el estado del motor en parada
   detenerMotores();
-  
+
   // Iniciamos el puerto serial
   Serial.begin(9600);
 }
@@ -27,7 +32,7 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     char t = Serial.read();
-    
+
     if (t == 'F') {
       moverAdelante();
     } else if (t == 'B') {
@@ -38,9 +43,21 @@ void loop() {
       girarDerecha();
     } else if (t == 'W') {
       encenderLed();
+    } else if (t == 'U') {
+      encenderLedRed();
+    } else if (t == 'u') {
+      apagarLedRed();
     } else if (t == 'w') {
       apagarLed();
-    } else if (t == 'S') {
+    } else if (t == 'V') {
+      bocinaON();
+    } else if (t == 'v') {
+      bocinaOFF();
+    } else if (t >= '1' && t <= '9') {
+      // Ajustar la velocidad del motor basado en el valor recibido
+      velocidad_motor = map(t - '0', 1, 9, 26, 255);
+    } else if (t == '0') {
+      // Detener los motores
       detenerMotores();
     }
   }
@@ -49,37 +66,52 @@ void loop() {
 void moverAdelante() {
   digitalWrite(driver_IN1, HIGH);
   digitalWrite(driver_IN3, HIGH);
-  analogWrite(driver_ENA, 255);
-  analogWrite(driver_ENB, 255);
+  analogWrite(driver_ENA, velocidad_motor);
+  analogWrite(driver_ENB, velocidad_motor);
 }
 
 void moverAtras() {
   digitalWrite(driver_IN1, LOW);
   digitalWrite(driver_IN3, LOW);
-  analogWrite(driver_ENA, 0);
-  analogWrite(driver_ENB, 0);
+  analogWrite(driver_ENA, velocidad_motor);
+  analogWrite(driver_ENB, velocidad_motor);
 }
 
 void girarIzquierda() {
   digitalWrite(driver_IN1, LOW);
   digitalWrite(driver_IN3, HIGH);
-  analogWrite(driver_ENA, 255);
-  analogWrite(driver_ENB, 255);
+  analogWrite(driver_ENA, velocidad_motor);
+  analogWrite(driver_ENB, velocidad_motor);
 }
 
 void girarDerecha() {
   digitalWrite(driver_IN1, HIGH);
   digitalWrite(driver_IN3, LOW);
-  analogWrite(driver_ENA, 255);
-  analogWrite(driver_ENB, 255);
+  analogWrite(driver_ENA, velocidad_motor);
+  analogWrite(driver_ENB, velocidad_motor);
 }
 
 void encenderLed() {
-  digitalWrite(9, HIGH);
+  digitalWrite(led, HIGH);
 }
 
 void apagarLed() {
-  digitalWrite(9, LOW);
+  digitalWrite(led, LOW);
+}
+void encenderLedRed() {
+  digitalWrite(ledRED, HIGH);
+}
+
+void apagarLedRed() {
+  digitalWrite(ledRED, LOW);
+}
+
+void bocinaON() {
+  digitalWrite(buzz, HIGH);
+}
+
+void bocinaOFF() {
+  digitalWrite(buzz, LOW);
 }
 
 void detenerMotores() {
